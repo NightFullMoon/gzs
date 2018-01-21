40b8458c3fe768b669768270f4113d1b80e0a931
NightFullMoon <2406799112@qq.com>
2018-01-21 21:41:29 +0800

    【button / 按钮】 增加了按钮disabled的prop

--- src/i-button.vue
+++ src/i-button.vue
@@ -1,68 +1,152 @@
 <template>
-    <a href="###" class="button round flat">
+    <button href="###" class="button" :class="{disabled:mDisabled}" @click="!mDisabled && $emit('click')">
         <slot></slot>
-    </a>
+    </button>
 </template>
 
 <script>
 export default {
-    name: "i-button"
-}
+  name: "i-button",
+  //   这块其实应该让生成器来生成才对，注释应该只写上说明
+  
+  /**@type {Object} props
+   * @property {Boolean|String} [disabled=false] - 是否要禁用此按钮，如果设置为`true` 或者`"disabled"`,或者`"true"`，则该按钮会被禁用。禁用状态下，`click`事件不会被触发。但是`click.native`事件不受此属性影响
+   **/
+
+  props: {
+    disabled: {
+      type: [Boolean, String],
+      default: false
+    }
+  },
+  computed: {
+    mDisabled: function() {
+      if ("boolean" === typeof this.disabled) {
+        return this.disabled;
+      } else if ("string" === typeof this.disabled) {
+        return "disabled" === this.disabled || "true" === this.disabled;
+      }
+    }
+  }
+};
 </script>
 
 <style lang="less" >
 @import (reference) "common.less";
-.Button(@height) {
-    .regular-text(); // height: @height;
-    line-height: @height;
-    display: inline-block; // font-size: @height * 0.5;
-    padding: 0px @height/2;
-    margin: @height/4;
-    text-decoration: none;
-    vertical-align: middle; //    border:1px solid #333;
-    // color:#333;
-    box-sizing: border-box;
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.4);
-    background-color: white;
-    text-align: center;
 
-    // 圆形按钮
-    &.round {
-        border-radius: @height/2;
-    }
+.Button-size(@height) {
+  @inner-height :@height - 2px;
 
-    &.flat{
-        .regular-border();
-         line-height: @height - 2 ;
-         box-shadow: none;
-        //  border:1px solid 
-    }
+  min-height: @height;
+  font-size:  @inner-height/2;
+  line-height: @inner-height/2;
+  padding: @inner-height/4 @height/2;
+  margin: @height/4;
+  border-radius: @height/2;
 
+  // 圆形按钮
+  &.round {
+    border-radius: @height/2;
+  }
+}
 
-    &.radius {
-        // border-radius: @height/2;
-        .radius-container();
-    }
+/** 
+* 颜色
+* 创建一个，实体风格的按钮
+* @function 
+* @param {Color} normal - 默认情况下按钮的颜色
+* 
+*/
+.Button-color-solid(@normal, @hover:darken(@normal, 10%),@active:darken(@normal, 20%), @disabled:#ccc) {
+  color: white;
+  background-color: @normal;
+  border: none;
+  &:hover,
+  &.hover{
+    background-color: @hover;
+  }
+  &:active,
+  &.active {
+    background-color: @active;
+  }
+
+  &:disabled,
+  &.disabled {
+    background-color: @disabled;
+  }
+}
+
+/** 
+* 颜色
+* 创建一个线框风格的button
+*/
+.Button-color-line(@normal, @hover:darken(@normal, 10%), @active:darken(@normal, 20%),@disabled:#ccc) {
+  color: @normal;
+
+  border: 1px solid transparent;
+  border-color: @normal;
+
+  &:hover,
+  &.hover
+   {
+    color: @hover;
+    border-color: @hover;
+  }
+  &:active,
+  &.active {
+    color: @active;
+    border-color: @active;
+  }
+
+  &:disabled,
+  &.disabled {
+    color: @disabled;
+    border-color: @disabled;
+  }
 }
 
 .button {
-    .Button(32px);
-    &.primary {
-        background-color: @theme-color;
-        color: white;
-        border: none;
-    }
-    &.warning {
-        background-color: red;
-        color: white;
-        border: none;
-    }
+//   .Button(32px);
+  .Button-size(32px);
 
-    &.xl {
-        // width: 80%;
-        // text-align: center;
-        // margin: 8px 8;
-        display: block;
-    }
+    //  .regular-text();
+  display: inline-block;
+  text-decoration: none;
+  vertical-align: middle;
+  box-sizing: border-box;
+  background-color: white;
+  text-align: center;
+  cursor: pointer;
+  outline: none;
+
+  &.radius {
+    .radius-container();
+  }
+
+
+
+  &.disabled:hover {
+    cursor: not-allowed;
+  }
+
+  .Button-color-line(#666,#333);
+  //   .round;
+
+  &.primary {
+    .Button-color-solid(@theme-color);
+  }
+
+  &.warning {
+    .Button-color-solid(red);
+  }
+
+  &.xl {
+    width: 100%;
+    display: block;
+  }
+}
+
+.button.test {
+  .Button-color-line(rgb(249, 38, 114));
 }
 </style>
