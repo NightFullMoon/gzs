e96712345808353bf1ffe5ddda40b6b1017fdfa2
NightFullMoon <2406799112@qq.com>
2018-01-21 17:31:55 +0800

    【alert / 弹窗】增加了注释；修复了弹窗上的按钮在没有传递action属性时报错的情况

--- src/i-alert/index.js
+++ src/i-alert/index.js
@@ -27,8 +27,24 @@ import Vue from "vue"
 import iDialog from "./i-dialog.vue"
 import iButton from "../i-button.vue"
 
+/**
+ * 创建一个按钮对象
+ * @typedef {Object} buttonObject
+ * @property {String} label - 按钮上显示的文本信息
+ * @property {String[]} [className=[]] - 字符串数组，该数组中所有的元素都会被作为css类名添加到组件最外层的元素上
+ * @property {Function} [action=function(){}] - 按钮被点击时触发的回调函数，在这个函数中，`this`指向的是`dialog对象`，因此可以通过`this`来调用dialog对象的接口；另外，默认情况下按钮在处理完回调事件后，**会自动关闭弹窗**，要阻止此行为，可以在回调函数中`return false`，然后调用`this.close();`来手动关闭弹窗;
+ */
 
-
+/**
+ * 弹出一个弹窗
+ * @param {String} arg - 弹窗的标题
+ * @param {Object} arg - 配置对象
+ * @param {String} [arg.title =""] - 弹窗的标题
+ * @param {String} [arg.content = ""] - 弹窗的内容
+ * @param {String[]} [arg.className = []] - String数组，该数组中所有的元素都会被作为css类名添加到组件最外层的元素上，可以借此来自定义主题或者覆盖掉默认样式
+ * @param {Function} [arg.onClose = function(){}] - 当弹窗被关闭时，调用的回调函数
+ * @param {buttonObject[]} [arg.buttons=[{label:"确定",className:["radius","primary"]}]] - button对象数组，该数组中所有的元素都将会被转换成button元素添加到弹窗。如果不指定这个值，默认情况下会存在一个“确定”按钮
+ */
 
 var Alert = function (arg) {
 
@@ -43,7 +59,8 @@ var Alert = function (arg) {
                     click: function () {
 
                         let dialog = vueObj.$refs.dialog;
-                        let shouldClose = element.action.call(dialog);
+                        // 没有传递action属性也是默认关闭
+                        let shouldClose = element.action?element.action.call(dialog):true;
 
                         if (false === shouldClose) {
                             return;
